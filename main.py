eebde04dd029f78f806e22c2a301699b04fcc4a4
Luan Nico <luanpotter27@gmail.com>
2020-04-26 15:48:15 -0400

    base route

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Welcome to Some Good News API!'
 
+@app.route('/update-news')
+def updateNews():
+    return "Update news route called. Yet to be implemented"
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
