030a50befba374348736da86f9b9093bfcb09430
Luan Nico <luanpotter27@gmail.com>
2020-04-26 16:20:29 -0400

    Improve deploy script to mention the file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -xe
 
+if [ ! -f secrets/sgn-test-firestore-access.json ]; then
+    echo "Please create file secrets/sgn-test-firestore-access.json before deploying."
+    exit 1
+fi
+
 gcloud app deploy
 gcloud app deploy cron.yaml
